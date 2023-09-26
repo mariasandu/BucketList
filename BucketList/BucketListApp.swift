b39f65ca3933d7e49092c3a4d0f1551234c21d70
Maria Sandu <Maria.Sandu@emids.com>
2023-09-26 12:37:49 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Maria Sandu on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
